6e8f40f6b9904dd880a36b44ba7deeee3688cc85
ghassen-create <ghassen.bougacha@gmail.com>
2022-02-21 09:55:42 +0100

    initial commit

---
+++ main.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+
+void tri_selection(int[],int);
+int schtroumpf(int[], int, int[], int);
+
+int main() {
+    const int n = 6, m = 7;
+    int T[n], T2[m], a;
+    printf("*********************************** Welcome to TP2 ***********************************\n");
+    printf("First fill the array with %d integers\n", n);
+    for (int i = 0; i < n; i++) {
+        printf("Enter element %d: ", i);
+        scanf("%d", &T[i]);
+    }
+    while (1){
+        printf("\nEnter number of exercise: ");
+        scanf("%d", &a);
+        switch (a) {
+            case 1: tri_selection(T, n); break;
+            case 2:
+                printf("Please fill the other array\n");
+                for (int i = 0; i < m; i++) {
+                    printf("Enter Element %d: ", i);
+                    scanf("%d", T2[i]);
+                }
+                printf("The schtroumpf is %d", schtroumpf(T, n, T2, m)); break;
+            default:
+                printf("Exercise does not exist"); break;
+        }
+    }
+}
+
+int minimum(int T[], int n, int i) {
+    int min = 0;
+    for (; i < n; i++) {
+        if(min < T[i])
+            min = i;
+    }
+    return min;
+}
+
+int permuter(int T[], int i, int j){
+    int aux = T[i];
+    T[i] = T[j];
+    T[j] = aux;
+    return T;
+}
+
+void write(int T[], int n){
+    for (int i = 0; i < n; i++) {
+        printf("%d ", T[i]);
+    }
+}
+
+void tri_selection(int T[],int n){
+    printf("\n******************************* Exercise 1: selection sort *******************************\n");
+    for (int i = 0; i < n; ++i) {
+        int j = minimum(T, n, i);
+        permuter(T, i, j);
+    }
+    write(T, n);
+}
+
+void fill(int T[], int n){
+    for (int i = 0; i < n; i++) {
+        printf("\nEnter Element %d: ", i);
+        scanf("%d", T[i]);
+    }
+}
+
+int schtroumpf(int T[], int n, int T2[], int m){
+    printf("\n******************************* Exercise 2: schtroumpf of 2 arrays *******************************\n");
+    int s = 0;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            s += T[i] * T2[j];
+        }
+    }
+
+    return s;
+}
